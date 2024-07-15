a7959414d039ebdbac0712383ed6d7b16d1290e9
Dikshantw <dikshantw11@gmail.com>
2024-07-15 17:25:05 +0530

    Navbar extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Tprimary: "#c3c3c3",
+        Tsecondary: "#797979",
+        Bgsecondary: "#1a1a1a",
+        Bgprimary: "#0e0e0e",
+        Bgnav: "#080808",
+      },
+    },
   },
   plugins: [],
 };
